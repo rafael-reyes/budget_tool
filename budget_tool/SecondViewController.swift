55f0ea7f5fefbfe0d81f0639c6829ec2aac49eec
Rafael Reyes <rafael_reyes@brown.edu>
2017-07-24 21:13:47 -0700

    Initial Commit

---
+++ budget_tool/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  budget_tool
+//
+//  Created by Rafael Reyes on 7/24/17.
+//  Copyright © 2017 Rafael Reyes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ budget_toolTests/budget_toolTests.swift
@@ -0,0 +1,36 @@
+//
+//  budget_toolTests.swift
+//  budget_toolTests
+//
+//  Created by Rafael Reyes on 7/24/17.
+//  Copyright © 2017 Rafael Reyes. All rights reserved.
+//
+
+import XCTest
+@testable import budget_tool
+
+class budget_toolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
