311711a5ea7435551a4dfef9ce4555c9b4420d0e
Rafael Reyes <rafael_reyes@brown.edu>
2017-08-02 22:27:56 -0700

    first view controller

--- budget_tool/FirstViewController.swift
+++ budget_tool/FirstViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var button1D: UIButton!
+    @IBOutlet weak var button1W: UIButton!
+    @IBOutlet weak var button2W: UIButton!
+    @IBOutlet weak var button1M: UIButton!
+    @IBOutlet weak var button3M: UIButton!
+    @IBOutlet weak var button1Y: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
